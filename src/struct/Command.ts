4bcb8dab0a790514f1146793fea06989d77d3b33
inkandnehal <sadmnehal@gmail.com>
2026-04-23 05:25:13 +0600

    fix(command): handle missing command metadata

--- src/struct/Command.ts
+++ src/struct/Command.ts
@@ -1,17 +1,23 @@
-import { ChatInputApplicationCommandData } from "discord.js";
+import { 
+    ChatInputCommandInteraction, 
+    RESTPostAPIChatInputApplicationCommandsJSONBody 
+} from "discord.js";
 import { TrestleClient } from "./TrestleClient";
 
 export class Command {
     public name: string;
     public description: string;
+    public data: RESTPostAPIChatInputApplicationCommandsJSONBody;
     public client!: TrestleClient;
+    public interaction!: ChatInputCommandInteraction;
 
-    constructor(data: ChatInputApplicationCommandData) {
+    constructor(data: RESTPostAPIChatInputApplicationCommandsJSONBody) {
         this.name = data.name;
-        this.description = data.description;    
+        this.description = data.description;  
+        this.data = data;  
     }
 
-    exec() {
+    exec(): void {
         throw new Error("Default 'exec' performed in " + this.name);
     }
 }
